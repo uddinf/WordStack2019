370e7476b6a77b61c2b46e1fba6a1ef943c4d37d
Demo User <demouser@n226-44.mtholyoke.edu>
2019-01-10 11:39:41 -0500

    Initial commit

---
+++ app/src/main/java/com/google/engedu/wordstack/MainActivity.java
@@ -0,0 +1,208 @@
+/* Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.engedu.wordstack;
+
+import android.content.res.AssetManager;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int WORD_LENGTH = 5;
+    public static final int LIGHT_BLUE = Color.rgb(176, 200, 255);
+    public static final int LIGHT_GREEN = Color.rgb(200, 255, 200);
+    private ArrayList<String> words = new ArrayList<>();
+    private Random random = new Random();
+    private StackedLayout stackedLayout;
+    private String word1, word2;
+    private static final String TAG = "WordActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AssetManager assetManager = getAssets();
+        try {
+            InputStream inputStream = assetManager.open("words.txt");
+            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            while((line = in.readLine()) != null) {
+                String word = line.trim();
+                if(word.length() == WORD_LENGTH){
+                    words.add(word);
+                }
+
+            }
+        } catch (IOException e) {
+            Toast toast = Toast.makeText(this, "Could not load dictionary", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        LinearLayout verticalLayout = (LinearLayout) findViewById(R.id.vertical_layout);
+        stackedLayout = new StackedLayout(this);
+        verticalLayout.addView(stackedLayout, 3);
+
+        View word1LinearLayout = findViewById(R.id.word1);
+        word1LinearLayout.setOnTouchListener(new TouchListener());
+        //word1LinearLayout.setOnDragListener(new DragListener());
+        View word2LinearLayout = findViewById(R.id.word2);
+        word2LinearLayout.setOnTouchListener(new TouchListener());
+        //word2LinearLayout.setOnDragListener(new DragListener());
+    }
+
+    private class TouchListener implements View.OnTouchListener {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN && !stackedLayout.empty()) {
+                LetterTile tile = (LetterTile) stackedLayout.peek();
+                tile.moveToViewGroup((ViewGroup) v);
+                if (stackedLayout.empty()) {
+                    TextView messageBox = (TextView) findViewById(R.id.message_box);
+                    messageBox.setText(word1 + " " + word2);
+                }
+                /**
+                 **
+                 **  YOUR CODE GOES HERE
+                 **
+                 **/
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private class DragListener implements View.OnDragListener {
+
+        public boolean onDrag(View v, DragEvent event) {
+            int action = event.getAction();
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    v.setBackgroundColor(LIGHT_BLUE);
+                    v.invalidate();
+                    return true;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    v.setBackgroundColor(LIGHT_GREEN);
+                    v.invalidate();
+                    return true;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    v.setBackgroundColor(LIGHT_BLUE);
+                    v.invalidate();
+                    return true;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    v.setBackgroundColor(Color.WHITE);
+                    v.invalidate();
+                    return true;
+                case DragEvent.ACTION_DROP:
+                    // Dropped, reassign Tile to the target Layout
+                    LetterTile tile = (LetterTile) event.getLocalState();
+                    tile.moveToViewGroup((ViewGroup) v);
+                    if (stackedLayout.empty()) {
+                        TextView messageBox = (TextView) findViewById(R.id.message_box);
+                        messageBox.setText(word1 + " " + word2);
+                    }
+                    /**
+                     **
+                     **  YOUR CODE GOES HERE
+                     **
+                     **/
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    public boolean onStartGame(View view) {
+        TextView messageBox = (TextView) findViewById(R.id.message_box);
+        messageBox.setText("Game started");
+
+        // randomly choose 2 words
+        Random rand = new Random();
+        int index1 = rand.nextInt(words.size());
+        int index2 = rand.nextInt(words.size());
+
+        // make sure words are not the same
+        while(index2==index1){
+            index2 = rand.nextInt(words.size());
+        }
+        // words to be used in game
+        word1 = words.get(index1);
+        word2 = words.get(index2);
+
+        int wordChoice = rand.nextInt(2) + 1;
+        int word1Counter = 0;
+        int word2Counter = 0;
+        String scrambledLetters = "";
+
+        // randomly choose word1 or word2 to get letters from to put into scrambled string
+        while(word1Counter != word1.length() || word2Counter != word2.length()) {
+            //randomly choose word
+            wordChoice = rand.nextInt(2) + 1;
+
+            if (wordChoice == 1 && word1Counter < word1.length()) {
+                scrambledLetters += word1.charAt(word1Counter);
+                word1Counter++;
+
+            } else if (wordChoice == 2 && word2Counter < word2.length()) {
+                scrambledLetters += word2.charAt(word2Counter);
+                word2Counter++;
+            }
+        }
+
+        char[] letters = scrambledLetters.toCharArray();
+
+        // update on screen with scrambled words
+        for(int i = letters.length - 1; i >= 0; i--){
+
+            // new tile for each letter
+            LetterTile tile = new LetterTile(this,letters[i]);
+
+            // push into stack layout
+            stackedLayout.push(tile);
+        }
+        Log.i(TAG, "word1: " + word1);
+        Log.i(TAG, "word2: " + word2);
+        Log.i(TAG, "word " + scrambledLetters + " " + word1 + " " + word2);
+        return true;
+    }
+
+
+    public boolean onUndo(View view) {
+        /**
+         **
+         **  YOUR CODE GOES HERE
+         **
+         **/
+        return true;
+    }
+}
---
+++ app/src/main/java/com/google/engedu/wordstack/StackedLayout.java
@@ -0,0 +1,68 @@
+/* Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.engedu.wordstack;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.Stack;
+
+public class StackedLayout extends LinearLayout {
+
+    private Stack<View> tiles = new Stack();
+
+    public StackedLayout(Context context) {
+        super(context);
+    }
+
+    public void push(View tile) {
+        /**
+         * call removeView with the tile on top of the stack (if there is one) to hide that tile
+         * push the specified tile onto the tiles stack
+         * call addView with the tile that was just pushed
+         */
+
+
+    }
+
+    public View pop() {
+        View popped = null;
+        /**
+         **pop a tile from tiles
+         * call removeView with it
+         * call addView with the tile that is now on top of the stack
+         * return the popped tile
+         **/
+        return popped;
+    }
+
+    public View peek() {
+        return tiles.peek();
+    }
+
+    public boolean empty() {
+        return tiles.empty();
+    }
+
+    public void clear() {
+        /**
+         **
+         **  YOUR CODE GOES HERE
+         **
+         **/
+    }
+}
